5ed8d7b0c5352feaa77ec28bc616f8a57cd4c00b
Mehdi Marzban <marzban7272@gmail.com>
2025-02-16 12:41:22 +0330

    fix: update card styles

--- src/components/features/Products.tsx
+++ src/components/features/Products.tsx
@@ -1,32 +1,34 @@
-import Link from 'next/link'
-import { Skeleton } from '../ui/skeleton'
-import ProductImage from './product/ProductImage'
+import Link from "next/link";
+import { Skeleton } from "../ui/skeleton";
+import ProductImage from "./product/ProductImage";
 
 type Props = {
-  products: Product[]
-}
+    products: Product[];
+};
 
 export const ProductsLoader = () => {
-  return Array.from({ length: 10 }).map((_, i) => (
-    <div key={i} className="flex flex-col gap-2">
-      <Skeleton className="aspect-square rounded-lg" />
-      <Skeleton className="h-3 w-2/3" />
-      <Skeleton className="h-3 w-1/3" />
-      <Skeleton className="h-8 w-16" />
-    </div>
-  ))
-}
+    return Array.from({ length: 10 }).map((_, i) => (
+        <div key={i} className="flex flex-col gap-2">
+            <Skeleton className="aspect-square rounded-lg" />
+            <Skeleton className="h-3 w-2/3" />
+            <Skeleton className="h-3 w-1/3" />
+            <Skeleton className="h-8 w-16" />
+        </div>
+    ));
+};
 
 export default ({ products }: Props) =>
-  products.map((p) => (
-    <Link href={`/products/${p.id}`} key={p.id} className="flex flex-col gap-2">
-      <ProductImage alt={p.title} image={p.images[0]} />
-      <div>
-        <h2 className="line-clamp-1 font-semibold text-sm">{p.title}</h2>
-        <p className="text-muted-foreground text-sm">${p.price}.00</p>
-        <p className="mt-2 w-fit rounded-md bg-muted/65 p-2 font-medium text-xs shadow-sm">
-          {p.category.name}
-        </p>
-      </div>
-    </Link>
-  ))
+    products.map((p) => (
+        <Link href={`/products/${p.id}`} key={p.id} className="flex flex-col gap-2 border-b border-muted pb-1">
+            <ProductImage alt={p.title} image={p.images[0]} />
+            <div>
+                <h2 className="line-clamp-1 font-semibold text-sm">{p.title}</h2>
+                <div className="flex flex-row justify-between items-center mt-1">
+                    <p className="text-muted-foreground text-sm">${p.price}.00</p>
+                    <p className="w-fit rounded-md bg-muted/65 p-2 font-medium text-xs shadow-sm">
+                        {p.category.name}
+                    </p>
+                </div>
+            </div>
+        </Link>
+    ));
