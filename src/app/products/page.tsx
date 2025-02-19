0a6ff7652fef28b2df5e56b47ad605fe1769f6ab
Mehdi Marzban <marzban7272@gmail.com>
2025-02-19 17:31:49 +0330

    style: improve formatting and consistency in loading and products page components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,17 @@
-import ProductList from '@/components/features/ProductList'
-import { Suspense } from 'react'
+import ProductList from "@/components/features/ProductList";
+import { Suspense } from "react";
 
 export const metadata = {
-  title: 'Products Page',
-}
+    title: "Products Page",
+};
 
 export default () => {
-  return (
-    <section className="container mx-auto animate-delay-100 animate-fade-up p-4">
-      <h1 className="font-bold text-xl md:font-black md:text-2xl">
-        All Products
-      </h1>
-      <Suspense>
-        <ProductList />
-      </Suspense>
-    </section>
-  )
-}
+    return (
+        <section className="container mx-auto animate-delay-100 animate-fade-up p-4">
+            <h1 className="font-bold text-xl md:font-black md:text-2xl">All Products</h1>
+            <Suspense>
+                <ProductList />
+            </Suspense>
+        </section>
+    );
+};
